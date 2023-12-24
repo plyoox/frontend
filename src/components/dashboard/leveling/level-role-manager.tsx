9a15670c1c5d895862651817032530358bce027b
JohannesIBK <mail@greuter.dev>
2023-12-24 17:43:37 +0100

    add setting for remove roles on level up

--- src/components/dashboard/leveling/level-role-manager.tsx
+++ src/components/dashboard/leveling/level-role-manager.tsx
@@ -1,5 +1,6 @@
 import { LevelingResponse } from "@/types/responses";
 import AddLevelRoleButton from "@/components/dashboard/leveling/add-level-role-button";
+import CustomSwitch from "@/components/custom-switch";
 import LevelRoleList from "@/components/dashboard/leveling/level-role-list";
 
 function LevelRoleManager({
@@ -11,6 +12,14 @@ function LevelRoleManager({
 }) {
   return (
     <>
+      <CustomSwitch
+        checked={config.remove_roles}
+        label={"Remove roles on level up"}
+        labelPosition={"left"}
+        onChange={(checked) => {
+          handleChange({ remove_roles: checked });
+        }}
+      />
       <LevelRoleList config={config} handleChange={handleChange} />
       <AddLevelRoleButton config={config} handleChange={handleChange} />
     </>
