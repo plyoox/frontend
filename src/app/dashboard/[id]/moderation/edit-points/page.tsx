a7afb5d159fa563e451d32d5892a31432f381ac2
JohannesIBK <mail@greuter.dev>
2023-12-26 21:44:05 +0100

    change wording

--- src/app/dashboard/[id]/moderation/edit-points/page.tsx
+++ src/app/dashboard/[id]/moderation/edit-points/page.tsx
@@ -7,8 +7,8 @@ function Page() {
     <div>
       <h1 className={"text-2xl font-semibold"}>Point punishments</h1>
       <Alert icon={<IconQuestionMark size={16} />} title="Info" variant="outline">
-        Decide what happens when a user reaches the maximum amount of 10 points. After being punished the points will be
-        reset to 0.
+        Decide what happens when a user reaches the maximum amount of 10 points. After being punished, the points will
+        be reset to 0.
         <br />
         Points can be added by rule punishments or manually by moderators.
       </Alert>
