81822eb98de4d70810a0284c4afa1b41cd0d2813
JohannesIBK <mail@greuter.dev>
2024-01-04 15:39:28 +0100

    use small font for information
    Signed-off-by: JohannesIBK <mail@greuter.dev>

--- src/components/dashboard/settings/webhook-container.tsx
+++ src/components/dashboard/settings/webhook-container.tsx
@@ -38,7 +38,7 @@ function WebhookContainer({ className }: { className?: string }) {
         ))}
 
         {config.length === 0 && (
-          <div className={"flex h-14 items-center justify-center rounded-md bg-mt-dark-7 text-mt-dark-2"}>
+          <div className={"flex h-14 items-center justify-center rounded-md bg-mt-dark-7 text-sm text-mt-dark-2"}>
             No webhooks created yet.
           </div>
         )}
