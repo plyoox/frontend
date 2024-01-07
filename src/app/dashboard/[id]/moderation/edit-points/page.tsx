03302cb5529adceabf6e5beef8cfefa69dfa16c9
JohannesIBK <mail@greuter.dev>
2024-01-07 13:52:06 +0100

    change info icon
    Signed-off-by: JohannesIBK <mail@greuter.dev>

--- src/app/dashboard/[id]/moderation/edit-points/page.tsx
+++ src/app/dashboard/[id]/moderation/edit-points/page.tsx
@@ -1,12 +1,12 @@
 import { Alert } from "@mantine/core";
-import { IconQuestionMark } from "@tabler/icons-react";
+import { IconInfoCircle } from "@tabler/icons-react";
 import EditPointsContainer from "@/components/dashboard/edit-points/edit-points-container";
 
 function Page() {
   return (
     <div>
       <h1 className={"text-2xl font-semibold"}>Point punishments</h1>
-      <Alert icon={<IconQuestionMark size={16} />} mb={10} title="Info" variant="light">
+      <Alert icon={<IconInfoCircle />} mb={10} variant="light">
         Decide what happens when a user reaches the maximum amount of 10 points. After being punished, the points will
         be reset to 0.
         <br />
