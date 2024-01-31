00dcc48f180188b16a4f158d13dc9a39f8c83cfb
JohannesIBK <mail@greuter.dev>
2024-01-31 14:17:42 +0100

    add padding for small screens
    Signed-off-by: JohannesIBK <mail@greuter.dev>

--- src/components/app-shell/header/components/login-button.tsx
+++ src/components/app-shell/header/components/login-button.tsx
@@ -7,7 +7,7 @@ function LoginButton() {
     <Link href={`${API_URL}/discord/login`}>
       <button
         className={
-          "hover:shadow-ring flex h-12 items-center justify-center gap-4 rounded-lg bg-pl-accent-dark text-white duration-200 hover:bg-pl-accent hover:shadow-pl-accent/80 sm:w-40"
+          "flex h-12 items-center justify-center gap-4 rounded-lg bg-pl-accent-dark px-4 text-white duration-200 hover:bg-pl-accent hover:shadow-ring hover:shadow-pl-accent/80 sm:w-40"
         }
       >
         <span className="hidden text-xl font-bold sm:block">Login</span>
