6fd888069d7caf1a091bfd8edad5452929a84f89
JohannesIBK <mail@greuter.dev>
2024-02-07 11:06:15 +0100

    remove headlessui
    Signed-off-by: JohannesIBK <mail@greuter.dev>

--- src/components/app-shell/header/components/user-view.tsx
+++ src/components/app-shell/header/components/user-view.tsx
@@ -1,13 +1,14 @@
 import { IconChevronDown, IconListSearch, IconLogout } from "@tabler/icons-react";
-import { Menu } from "@headlessui/react";
+import { Menu } from "@mantine/core";
 import { UserStoreContext } from "@/stores/user-store";
 import { observer } from "mobx-react-lite";
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 function UserView() {
   const store = useContext(UserStoreContext);
+  const [open, setOpen] = useState(false);
 
   const avatar = useMemo(() => {
     const user = store.user!;
@@ -18,67 +19,61 @@ function UserView() {
   }, [store.user]);
 
   return (
-    <Menu as="div" className="relative">
-      {({ open }) => (
-        <>
-          <Menu.Button
-            className={`flex items-center justify-center rounded-lg bg-transparent p-1.5 px-2 outline-none duration-200 hover:bg-mt-dark-7 hover:shadow-pl-button ${
-              open ? "shadow-pl-button" : ""
-            }`}
-          >
-            <Image
-              unoptimized
-              alt="avatar"
-              className="mr-2 rounded-full"
-              height={36}
-              src={`https://cdn.discordapp.com/${avatar}`}
-              width={36}
-            />
+    <Menu onChange={setOpen} opened={open}>
+      <Menu.Target>
+        <button
+          className={`flex items-center justify-center rounded-lg bg-transparent p-1.5 px-2 outline-none duration-200 hover:bg-mt-dark-7 hover:shadow-pl-button ${
+            open ? "shadow-pl-button" : ""
+          }`}
+        >
+          <Image
+            unoptimized
+            alt="avatar"
+            className="mr-2 rounded-full"
+            height={36}
+            src={`https://cdn.discordapp.com/${avatar}`}
+            width={36}
+          />
 
-            <span className={"text-white"}>{store.user?.display_name ?? store.user?.username}</span>
+          <span className={"text-white"}>{store.user?.display_name ?? store.user?.username}</span>
 
-            <IconChevronDown className={`ml-1 duration-200 ${open ? "rotate-180" : ""}`} size={15} />
-          </Menu.Button>
+          <IconChevronDown className={`ml-1 duration-200 ${open ? "rotate-180" : ""}`} size={15} />
+        </button>
+      </Menu.Target>
 
-          <Menu.Items
-            className="absolute right-0 z-20 mt-2 w-48 rounded-md bg-mt-dark-7 p-2 text-sm text-gray-200 shadow-pl-accent-light drop-shadow-2xl"
-            style={{ left: "-17px" }}
-          >
-            <Menu.Item
-              as={Link}
-              className="flex cursor-pointer rounded-md p-2 hover:bg-mt-dark-6 hover:text-white"
-              href="/dashboard"
-            >
-              <IconListSearch className="mr-2" size={18} />
-              Server list
-            </Menu.Item>
+      <Menu.Dropdown className="p-2 shadow-pl-accent-light drop-shadow-2xl">
+        <Menu.Label>Server</Menu.Label>
+        <Menu.Item component={Link} href="/dashboard" leftSection={<IconListSearch className={"size-3.5"} />}>
+          Server list
+        </Menu.Item>
 
-            <Menu.Item
-              as={Link}
-              className="mt-0.5 flex cursor-pointer rounded-md p-2 duration-200 hover:bg-red-400/20 hover:text-white"
-              href="/"
-              onClick={() => {
-                store.requestLogout(false);
-              }}
-            >
-              <IconLogout className="mr-2 text-red-400" size={18} />
-              Logout
-            </Menu.Item>
+        <Menu.Divider />
 
-            <Menu.Item
-              as={Link}
-              className="mt-0.5 flex cursor-pointer rounded-md p-2 duration-200 hover:bg-red-400/20 hover:text-white"
-              href="/"
-              onClick={() => {
-                store.requestLogout(true);
-              }}
-            >
-              <IconLogout className="mr-2 text-red-400" size={18} />
-              Logout everywhere
-            </Menu.Item>
-          </Menu.Items>
-        </>
-      )}
+        <Menu.Label>User</Menu.Label>
+        <Menu.Item
+          color={"red"}
+          component={Link}
+          href="/"
+          leftSection={<IconLogout className={"size-3.5"} />}
+          onClick={() => {
+            store.requestLogout(false);
+          }}
+        >
+          Logout
+        </Menu.Item>
+
+        <Menu.Item
+          color={"red"}
+          component={Link}
+          href="/"
+          leftSection={<IconLogout className={"size-3.5"} />}
+          onClick={() => {
+            store.requestLogout(true);
+          }}
+        >
+          Logout everywhere
+        </Menu.Item>
+      </Menu.Dropdown>
     </Menu>
   );
 }
