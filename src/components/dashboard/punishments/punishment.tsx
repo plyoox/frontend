820670a17f590563667fd9246f49ddb8d4467e33
JohannesIBK <mail@greuter.dev>
2024-02-07 22:52:46 +0100

    handle to long names
    Signed-off-by: JohannesIBK <mail@greuter.dev>

--- src/components/dashboard/punishments/punishment.tsx
+++ src/components/dashboard/punishments/punishment.tsx
@@ -12,19 +12,19 @@ function Punishment({
   onEdit: () => void;
 }) {
   return (
-    <div className={"mt-2 flex items-center rounded-md bg-mt-dark-6 px-4 py-2"}>
-      <div className={"flex items-center gap-2"}>
+    <div className={"mt-2 flex items-center gap-x-4 rounded-md bg-mt-dark-6 px-4 py-2"}>
+      <div className={"flex flex-grow items-center gap-2"}>
         <Tooltip label={punishment.enabled ? "Enabled" : "Disabled"}>
           <span
-            className={`size-4 cursor-help rounded-full ${punishment.enabled ? "bg-green-500" : "bg-red-500"}`}
+            className={`size-4 min-w-4 cursor-help rounded-full ${punishment.enabled ? "bg-green-500" : "bg-red-500"}`}
             title={`Rule ${punishment.enabled ? "enabled" : "disabled"}`}
           />
         </Tooltip>
 
-        <span>{punishment.name}</span>
+        <span className={"line-clamp-2 text-wrap break-all"}>{punishment.name}</span>
       </div>
 
-      <div className={"ml-10 flex flex-col"}>
+      <div className={"mr-5 flex flex-col"}>
         <span className={"text-sm text-mt-dark-1"}>Actions</span>
         <span>{punishment.actions.length}</span>
       </div>
